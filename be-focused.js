846608a8c0e3dadaaf1c753dd1593259b62d24b3
Bruce Anderson <anderson.bruce.b@gmail.com>
2022-10-27 19:35:31 -0400

    0.0.4

--- be-focused.js
+++ be-focused.js
@@ -1,26 +1,21 @@
 import { register } from 'be-hive/register.js';
 import { define } from 'be-decorated/DE.js';
 export class BeFocused {
-    intro(proxy, target, beDecorProps) {
-        proxy.focus();
+    intro(proxy, self) {
+        self.focus();
     }
 }
 const tagName = 'be-focused';
-const ifWantsToBe = 'focused';
-const upgrade = '*';
 define({
     config: {
         tagName,
         propDefaults: {
-            ifWantsToBe,
-            upgrade,
             virtualProps: [],
             intro: 'intro',
         },
-        actions: {}
     },
     complexPropDefaults: {
         controller: BeFocused
     }
 });
-register(ifWantsToBe, upgrade, tagName);
+register('focused', '*', tagName);
--- be-focused.ts
+++ be-focused.ts
@@ -3,35 +3,23 @@ import {define, BeDecoratedProps} from 'be-decorated/DE.js';
 import {Actions, ProxyProps, VirtualProps, Proxy} from './types'; 
 
 export class BeFocused implements Actions{
-    intro(proxy: Proxy, target: HTMLElement, beDecorProps: BeDecoratedProps<any, any>): void {
-        proxy.focus();
+    intro(proxy: Proxy, self: HTMLElement): void {
+        self.focus();
     }
 }
 
-export interface BeFocused extends ProxyProps{}
-
 const tagName = 'be-focused';
-
-const ifWantsToBe = 'focused';
-
-const upgrade = '*';
-
 define<ProxyProps & BeDecoratedProps<ProxyProps, Actions>, Actions>({
     config:{
         tagName,
         propDefaults:{
-            ifWantsToBe,
-            upgrade,
             virtualProps: [],
             intro: 'intro',
         },
-        actions:{
-
-        }
     },
     complexPropDefaults:{
         controller: BeFocused
     }
 });
 
-register(ifWantsToBe, upgrade, tagName);
+register('focused', '*', tagName);
